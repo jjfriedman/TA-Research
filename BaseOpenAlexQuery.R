09192b7483221e3b59ca1ddd46cb3a31cdce9e51
Jason Friedman <jjasonf@gmail.com>
2025-08-22 16:52:22 -0600

    Now generates a series of CSVs: works, authors, corresponding authors, institutional corresponding authors, and institutional APC list.

--- BaseOpenAlexQuery.R
+++ BaseOpenAlexQuery.R
@@ -1,14 +1,24 @@
 #This code is based on the code developed by Teresa Schultz for her FSCI 2025 Workshop available here: https://github.com/schauch/OpenAlexRFSCI2025
 
-OutputPath = "OutputFiles/Institutional_2023DataRawFlattenedTest.csv" #File path for results
+#Install and include necessary packages. Install only needs to be run once.
+#install.packages("openalexR") 
+#install.packages("tidyverse")
+
+library(openalexR)
+library(tidyverse)
+
+OutputPath = "OutputFiles/USask2023/" #File path for results
 
 options(openalexR.mailto = "jason.friedman@usask.ca") #Add email address to use polite pool / Only needs to be done once
+
+Institution_OpenAlex_ID = "I32625721" #OpenAlex Institution ID is USask
+InstitutionFilterString = paste0("https://openalex.org/", Institution_OpenAlex_ID) #Adds OpenAlex URL string to institution ID for filtering
 
 #Query String
 
 Institutional_Works <- oa_fetch(
   entity = "works", #query type
-  authorships.institutions.lineage = "i32625721", #OpenAlex Institution ID is USask
+  authorships.institutions.lineage = Institution_OpenAlex_ID, 
   type = "article", #type of item
   primary_location.source.type = "journal", #limit to journal articles
   from_publication_date = "2023-01-01", #start range
@@ -39,13 +49,35 @@ FilterColumns = c(
   'grants'
   )
 
-Institutional_FilteredWorks <- Institutional_Works %>% select(-any_of(FilterColumns))
+#Generate initial works list
+Institutional_FilteredWorks <- Institutional_Works %>% select(-any_of(FilterColumns)) #Removes columns indicated above
+Institutional_FilteredWorksOnly <- Institutional_FilteredWorks %>% select(-any_of(c("authorships","apc"))) #Removes nested data authorships/apc for a clean works list for export
+write.csv(Institutional_FilteredWorksOnly,paste0(OutputPath,"WorksList.csv")) #Exports works list
 
-Institutional_Authors <- unnest(Institutional_FilteredWorks, authorships, names_sep = "_")
-Institutional_AuthorsAffilations <- unnest(Institutional_Authors, authorships_affiliations, names_sep = "_")
-Institutional_APCs <- unnest(Institutional_AuthorsAffilations, apc, names_sep = "_")
+#Generate initial authors list
+Institutional_Authors <- unnest(Institutional_FilteredWorks, authorships, names_sep = "_") #Unnests authorships so that all authors are listed
+Institutional_AuthorsOnly <- Institutional_Authors %>% select(-any_of(c("authorships_affiliations","apc"))) #Removes nested data affiliations and apc for a clearn authors list for export
+write.csv(Institutional_AuthorsOnly,paste0(OutputPath,"AuthorsList.csv")) #Exports authors list
+
+#Generate corresponding authors list
+Corresponding_only <- filter(Institutional_Authors, authorships_is_corresponding == "TRUE") #Filters list to only include corresponding authors
+Corresponding_AuthorsAffilations <- unnest(Corresponding_only, authorships_affiliations, names_sep = "_") #Unnests affiliations so that all affiliations are listed
+Corresponding_AuthorsAffilations_NoAPC <- Corresponding_AuthorsAffilations %>% select(!"apc") #Removes APC data for clean export
+write.csv(Corresponding_AuthorsAffilations_NoAPC,paste0(OutputPath,"CorrespondingAuthorsList.csv")) #Exports corresponding authors list
+
+#Generate institutional corresponding authors list
+Institutional_CorrespondingAuthors <- filter(Corresponding_AuthorsAffilations, authorships_affiliations_id == paste0("https://openalex.org/", Institution_OpenAlex_ID)) #Filters corresponding authors list 
+Institutional_CorrespondingAuthors_NoAPC <- Institutional_CorrespondingAuthors %>% select(!"apc") #Removes APC data for clean export
+write.csv(Institutional_CorrespondingAuthors_NoAPC,paste0(OutputPath,"InstitutionalCorrespondingAuthors.csv")) #Exports institutional corresponding authors list
+
+#Generate Paid/List APC list
+#Institutional_OA <- filter(Institutional_AuthorsOnly, is_oa == "TRUE")
+Institutional_APCs <- unnest(Institutional_CorrespondingAuthors, apc, names_sep = "_") #Unnests APC data for export
+#Corresponding_only <- filter(Institutional_APCs, authorships_is_corresponding == "TRUE")
 
 # Institutional_APCs <- Institutional_APCs %>% select(-any_of('authorships_affiliation_raw'))
 
-write.csv(Institutional_APCs, OutputPath)
+write.csv(Institutional_APCs, paste0(OutputPath,"InstitutionalAPCList.csv")) #Writes institutional corresponding author APC data
+#write.csv(Corresponding_only, CorrespondingOutputPath)
+
                                                     
